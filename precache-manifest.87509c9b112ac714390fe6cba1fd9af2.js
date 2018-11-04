a8c9df6ee2cff224434c7deb58117a0590190bec
Colin Reilly <colinrlly@gmail.com>
2018-11-03 23:48:45 -0400

    Updates

---
+++ precache-manifest.87509c9b112ac714390fe6cba1fd9af2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b18a0c3f4ee29df3e23e",
+    "url": "/mvp-ui/static/js/runtime~main.b18a0c3f.js"
+  },
+  {
+    "revision": "d5aa4e6a61c264ea4925",
+    "url": "/mvp-ui/static/js/main.d5aa4e6a.chunk.js"
+  },
+  {
+    "revision": "665dc639204940dc05d1",
+    "url": "/mvp-ui/static/js/1.665dc639.chunk.js"
+  },
+  {
+    "revision": "d5aa4e6a61c264ea4925",
+    "url": "/mvp-ui/static/css/main.c3ed4ee9.chunk.css"
+  },
+  {
+    "revision": "c70ec9635010b767edcd0b8bbfd0dbd5",
+    "url": "/mvp-ui/index.html"
+  }
+];
